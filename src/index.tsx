a1bed7d057a7d1ae8f930b87e25c58b0d9b8a9fb
limgyumin <52942566+limgyumin@users.noreply.github.com>
2021-07-09 21:31:08 +0900

    ADD: Redux + Redux Saga 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "styles/GlobalStyles";
 import { theme } from "styles/theme";
 import Root from "./components/Root";
 import reportWebVitals from "./reportWebVitals";
+import store from "modules";
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <Root />
+        <Provider store={store}>
+          <Root />
+        </Provider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
